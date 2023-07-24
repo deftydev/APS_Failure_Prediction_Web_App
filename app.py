db8225089535968a881589bdf633e8145d0a5d5d
deftydev <devanshgupta79212346@gmail.com>
2023-07-24 18:37:34 +0530

    Flask App

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request, send_file
+from src.exception import CustomException
+from src.logger import logging as lg
+import os,sys
+
+from src.pipeline.prediction_pipeline import PredictionPipeline
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to my application"
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def upload():
+    
+    try:
+
+
+        if request.method == 'POST':
+            # it is a object of prediction pipeline
+            prediction_pipeline = PredictionPipeline(request)
+            
+            #now we are running this run pipeline method
+            prediction_file_detail = prediction_pipeline.run_pipeline()
+
+            lg.info("prediction completed. Downloading prediction file.")
+            return send_file(prediction_file_detail.prediction_file_path,
+                            download_name= prediction_file_detail.prediction_file_name,
+                            as_attachment= True)
+
+
+        else:
+            return render_template('upload_file.html')
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+#execution will start from here
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug= True)
